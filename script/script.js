af870d562c8a117d2f7d43e756e3626aa5db0dbc
Nikolay <laleniko@gmail.com>
2019-02-21 10:26:34 +0300

    correct js 3

--- script/script.js
+++ script/script.js
@@ -12,13 +12,13 @@ class Game {
 	}
 
 	createEmojy() {
+		this.deleteEmojy();
 		if (this.lives <= 0) {
 			let elem = document.querySelectorAll(".game-zone__popup-gameover");
 			let scores = document.querySelectorAll(".game-over__score");
 			scores[0].innerHTML = this.score;
 			elem[0].classList.remove('active');
 		} else {
-			this.deleteEmojy();
 			let randomAnimal = Math.floor(Math.random() * 9);
 			let randomZone = Math.floor(Math.random() * 5);
 			let elem = this.emojisArr[randomAnimal];
@@ -66,22 +66,36 @@ class Game {
 	}
 
 	isMouses() {
-		if (this.emojiNow === '🐭') {
+		if (this.emojiNow !== '🐭') {
 			this.score += 10;
 			this.allMouse +=1;
 			this.innerScores();
 			this.gameSpeed();
+			this.deleteEmojy();
 
 		} else 
 			this.lives -= 1;
 			this.innerLives();
+			this.deleteEmojy();
+	}
+
+	clearGame() {
+		this.score = 0;
+		this.lives = 3;
+		this.level = 1;
+		this.speed = 2500;
+		this.emojiNow =  '🐭';
+		this.allMouse = 0;
+		this.innerLives();
+		this.innerScores();
+		let elem = document.querySelectorAll(".game-header__star-value");
+		elem[0].innerHTML = this.level;
+
 	}
 
 	emojyDance() {
-		let zone = document.querySelectorAll(".game-zone__hole");
-		for (var i = 0; i < zone.length; i++) {
-			zone[i].addEventListener('click', () => this.isMouses());
-		}
+		clearInterval(this.interval);
+		this.clearGame();
 		this.createEmojy();
 	}
 	
@@ -91,9 +105,17 @@ let game = new Game;
 
 let rulesOpen = document.querySelectorAll('.game-header__help');
 let rulesClose = document.querySelectorAll('.game-zone__popup-button_help');
+let popup = document.querySelectorAll('.game-zone__popup-gameover')
+let popupClose = document.querySelectorAll('.game-zone__popup-button_game-over');
 let rules = document.querySelectorAll('.game-zone__popup-help');
 let gameStart = document.querySelectorAll('.game-header__start');
 
 gameStart[0].addEventListener('click',() => {game.emojyDance()});
 rulesOpen[0].addEventListener('click', () => {rules[0].classList.remove('active')});
-rulesClose[0].addEventListener('click', () => {rules[0].classList.add('active')})
+rulesClose[0].addEventListener('click', () => {rules[0].classList.add('active')});
+popupClose[0].addEventListener('click', () => popup[0].classList.add('active'));
+
+let zone = document.querySelectorAll(".game-zone__hole");
+		for (var i = 0; i < zone.length; i++) {
+			zone[i].addEventListener('click', () => game.isMouses());
+		}
